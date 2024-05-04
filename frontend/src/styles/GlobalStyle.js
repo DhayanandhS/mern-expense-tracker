0a025f444eba84438202f5c9bcbb66e7300505e1
DhayanandhS <dhaya.pit180@gmail.com>
2024-05-04 08:50:03 +0530

    added all files

--- frontend/src/styles/GlobalStyle.js
+++ frontend/src/styles/GlobalStyle.js
@@ -1,10 +1,20 @@
 import {createGlobalStyle} from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-    *  {
+    *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         list-style: none:
-    }       
+    }     
+    
+    :root{
+        --primary-color: #222260;
+        --primary-color2: 'color: rgba(34, 34, 96, .6)';
+        --primary-color3: 'color: rgba(34, 34, 96, .4)';
+        --color-green: #42AD00;
+        --color-grey: #aaa;
+        --color-accent: #F56692;
+        --color-delete: #FF0000;
+    }
 `;
