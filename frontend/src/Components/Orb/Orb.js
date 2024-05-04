4e954e995b52ba50074f04729fb8a86d3f0c6dbb
DhayanandhS <dhaya.pit180@gmail.com>
2024-05-04 22:17:02 +0530

    added new files

--- frontend/src/Components/Orb/Orb.js
+++ frontend/src/Components/Orb/Orb.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import styled, {keyframes} from 'styled-components'
+import {useWindowSize} from '../../utils/useWindowSize'
+
 
 function Orb() {
+
+    const {width, height} = useWindowSize()
+
+    console.log(width, height)
 
     const moveOrb = keyframes`
         0%{
             transform: translate(0, 0);
         }
         50%{
-            transform: translate(400px, 500px);
+            transform: translate(${width/1.2}px, ${height/2}px);
         }
         100%{
             transform: translate(0, 0);
@@ -23,7 +29,7 @@ function Orb() {
         margin-left: -37vh;
         margin-top: -37vh;
         background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
-        filter: blur(100px);
+        filter: blur(300px);
         animation: ${moveOrb} 5s alternate linear infinite;
     `;
     return (
