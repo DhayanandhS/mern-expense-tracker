7056ccd84ea8dad1dd899e6807ba4193ed942bc6
DhayanandhS <dhaya.pit180@gmail.com>
2024-05-17 10:46:15 +0530

    added Expenses

--- frontend/src/Components/Expenses/Expenses.js
+++ frontend/src/Components/Expenses/Expenses.js
@@ -1,19 +1,83 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layouts';
+import { useGlobalContext } from '../../context/globalContext';
+import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
+
 
 function Expenses(){
+
+    const {addIncome,incomes,getIncomes,deleteIncome,totalIncome} = useGlobalContext()
+
+    useEffect(() =>{
+        getIncomes()
+    }, [])
+
     return(
-        <ExpensesStyled>
+        <IncomeStyled>
             <InnerLayout>
-                Expense
+            <h1>Expenses</h1>
+            <h2 className="total-income">Total Expenses: <span>${totalIncome()}</span></h2>
+                <div className="income-content">
+                    <div className="form-container">
+                        <Form/>
+                    </div>
+                    <div className="incomes">
+                        {incomes.map((income) => {
+                            const {_id, title, amount, date, category, description, type} = income;
+                            return <IncomeItem
+                                key={_id}
+                                id={_id} 
+                                title={title} 
+                                description={description} 
+                                amount={amount} 
+                                date={date} 
+                                type={type}
+                                category={category} 
+                                indicatorColor="var(--color-green)"
+                                deleteItem={deleteIncome}
+                            />
+                        })}
+                    </div>
+                </div>
             </InnerLayout>
-        </ExpensesStyled>
+        </IncomeStyled>
     )
 }
 
-const ExpensesStyled = styled.div`
-
+const IncomeStyled = styled.div`
+    display: flex;
+    overflow: auto;
+    .total-income{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #FCF6F9;
+        border: 2px solid #FFFFFF;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        border-radius: 20px;
+        padding: 1rem;
+        margin: 1rem 0;
+        font-size: 2rem;
+        gap: .5rem;
+        span{
+            font-size: 2.5rem;
+            font-weight: 800;
+            color: var(--color-green);
+        }
+    }
+    .income-content{
+        display: flex;
+        gap: 2rem;
+        .incomes{
+            flex: 1;
+        }
+    }
+    .form-container{
+        width: 250px;
+    }
+    
 `;
 
 export default Expenses
