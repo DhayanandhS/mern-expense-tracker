ff57705313a17a19cd9ba00ea3c99925a08c0367
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-11 10:22:10 +0530

    added salary items

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -5,10 +5,12 @@ import { InnerLayout } from '../../styles/Layouts';
 import Chart from '../Chart/Chart.js';
 import { useGlobalContext } from '../../context/globalContext.js';
 import { dollar} from '../../utils/Icons.js'
+import History from '../History/History.js'; 
+
 
 
 function Dashboard(){
-    const {totalExpenses,totalIncome,totalBalance,getIncomes, getExpenses} = useGlobalContext()
+    const {totalExpenses,totalIncome,totalBalance,getIncomes, getExpenses,incomes, expenses} = useGlobalContext()
     useEffect(() => {
         getIncomes()
         getExpenses()
@@ -41,7 +43,27 @@ function Dashboard(){
                                 </div>
                             </div>
                         </div>
-
+                        <div className="history-con">
+                        <History />
+                        <h2 className="salary-title">Min <span>Salary</span>Max</h2>
+                        <div className="salary-item">
+                            <p>
+                                ${Math.min(...incomes.map(item => item.amount))}
+                            </p>
+                            <p>
+                                ${Math.max(...incomes.map(item => item.amount))}
+                            </p>
+                        </div>
+                        <h2 className="salary-title">Min <span>Expense</span>Max</h2>
+                        <div className="salary-item">
+                            <p>
+                                ${Math.min(...expenses.map(item => item.amount))}
+                            </p>
+                            <p>
+                                ${Math.max(...expenses.map(item => item.amount))}
+                            </p>
+                        </div>
+                    </div>
                     </div>
             </InnerLayout>
         </DashboardStyled>
