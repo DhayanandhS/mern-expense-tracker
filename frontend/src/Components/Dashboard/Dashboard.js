e0d87324c61d8fe25102e5edd852e4004c9526cb
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-11 10:24:41 +0530

    added styles for Dashboard

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -71,7 +71,78 @@ function Dashboard(){
 }
 
 const DashboardStyled = styled.div`
+    .stats-con{
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        gap: 2rem;
+        .chart-con{
+            grid-column: 1 / 4;
+            height: 400px;
+            .amount-con{
+                display: grid;
+                grid-template-columns: repeat(4, 1fr);
+                gap: 2rem;
+                margin-top: 2rem;
+                .income, .expense{
+                    grid-column: span 2;
+                }
+                .income, .expense, .balance{
+                    background: #FCF6F9;
+                    border: 2px solid #FFFFFF;
+                    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+                    border-radius: 20px;
+                    padding: 1rem;
+                    p{
+                        font-size: 3.5rem;
+                        font-weight: 700;
+                    }
+                }
 
+                .balance{
+                    grid-column: 2 / 4;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    p{
+                        color: var(--color-green);
+                        opacity: 0.6;
+                        font-size: 4.5rem;
+                    }
+                }
+            }
+        }
+
+        .history-con{
+            grid-column: 4 / -1;
+            h2{
+                margin: 1rem 0;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            }
+            .salary-title{
+                font-size: 1.2rem;
+                span{
+                    font-size: 1.8rem;
+                }
+            }
+            .salary-item{
+                background: #FCF6F9;
+                border: 2px solid #FFFFFF;
+                box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+                padding: 1rem;
+                border-radius: 20px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                p{
+                    font-weight: 600;
+                    font-size: 1.6rem;
+                }
+            }
+        }
+    }
 `;
 
 export default Dashboard
